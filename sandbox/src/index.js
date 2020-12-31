523b69797192d594a5c09976d913d974e070c1e2
Andrew J Carter <andrew.jeremiah.carter@gmail.com>
2020-12-30 18:26:36 -0700

    Creates sandbox page

---
+++ sandbox/src/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
